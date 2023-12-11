980e70a010adde57d80a98940a8506f6edcce87b
JELEE\User <jelee55>
2023-12-11 09:13:17 +0900

    order수정

--- workspace/rushwash/src/main/java/com/rushwash/app/order/dao/OrderDao.java
+++ workspace/rushwash/src/main/java/com/rushwash/app/order/dao/OrderDao.java
@@ -14,16 +14,16 @@ import com.rushwash.app.order.vo.OrderVo;
 public class OrderDao {
 
 	public List<OrderVo> getorderList(Connection conn, String memberNo) throws Exception {
-		
-		String sql = "SELECT O.NO ,O.PRICE ,O.PAYMENT_DATE ,O.RECEIVE_DATE ,O.DEL_YN ,M.NAME AS MEMBER_NAME ,M.ADDRESS ,M.TEL ,I.NAME AS ITEM ,L.EA ,S.STATUS AS LAUNDRY_STATUS ,T.STATUS AS ORDER_STATUS FROM LAUNDRY L JOIN LAUNDRY_ORDER O ON O.NO = L.ORDER_NO JOIN ORDER_STATUS T ON O.ORDER_STATUS = T.NO JOIN MEMBER M ON O.MEMBER_NO = M.NO JOIN LAUNDRY_STATUS S ON L.LAUNDRY_STATUS = S.NO JOIN ITEM I ON L.ITEM_NO = I.NO WHERE M.NO = ?";
+
+		String sql = "SELECT O.NO ,I.PRICE ,O.PAYMENT_DATE ,O.RECEIVE_DATE ,O.DEL_YN ,M.NAME AS MEMBER_NAME ,M.ADDRESS ,M.TEL ,I.NAME AS ITEM ,L.EA ,S.STATUS AS LAUNDRY_STATUS ,T.STATUS AS ORDER_STATUS FROM LAUNDRY L JOIN LAUNDRY_ORDER O ON O.NO = L.ORDER_NO JOIN ORDER_STATUS T ON O.ORDER_STATUS = T.NO JOIN MEMBER M ON O.MEMBER_NO = M.NO JOIN LAUNDRY_STATUS S ON L.LAUNDRY_STATUS = S.NO JOIN ITEM I ON L.ITEM_NO = I.NO WHERE M.NO = ?";
 		PreparedStatement pstmt = conn.prepareStatement(sql);
 		pstmt.setString(1, memberNo);
 		ResultSet rs = pstmt.executeQuery();
-		
+
 		List<OrderVo> orderVoList = new ArrayList<OrderVo>();
-		while(rs.next()) {
-			
-			String no = rs.getString("NO"); 
+		while (rs.next()) {
+
+			String no = rs.getString("NO");
 			String price = rs.getString("PRICE");
 			String paymentDate = rs.getString("PAYMENT_DATE");
 			String receiveDate = rs.getString("RECEIVE_DATE");
@@ -35,7 +35,7 @@ public class OrderDao {
 			String ea = rs.getString("EA");
 			String laundryStatus = rs.getString("LAUNDRY_STATUS");
 			String orderStatus = rs.getString("ORDER_STATUS");
-			
+
 			OrderVo vo = new OrderVo();
 			vo.setNo(no);
 			vo.setPrice(price);
@@ -49,14 +49,42 @@ public class OrderDao {
 			vo.setEa(ea);
 			vo.setLaundryStatus(laundryStatus);
 			vo.setOrderStatus(orderStatus);
-			
+
 			orderVoList.add(vo);
 		}
-		
+
+		JDBCTemplate.close(rs);
+		JDBCTemplate.close(pstmt);
+
+		return orderVoList;
+	}
+
+	public OrderVo getorderDetail(Connection conn, String memberNo) throws Exception {
+
+		String sql = "SELECT I.NAME AS ITEM ,O.PRICE ,L.EA ,O.PAYMENT_DATE ,O.EXP_DATE ,T.STATUS AS LAUNDRY_STATUS ,M.NAME AS MEMBER_NAME ,M.TEL ,O.ADDRESS ,O.REQUEST FROM MEMBER M JOIN LAUNDRY_ORDER O ON M.NO = O.MEMBER_NO JOIN ORDER_STATUS T ON O.ORDER_STATUS = T.NO JOIN LAUNDRY L ON T.NO = L.ORDER_NO JOIN ITEM I ON L.ITEM_NO = I.NO WHERE O.NO = ?";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, memberNo);
+		ResultSet rs = pstmt.executeQuery();
+
+		OrderVo vo = null;
+
+		if (rs.next()) {
+			vo = new OrderVo();
+			vo.setItem(rs.getString("ITEM"));
+			vo.setPrice(rs.getString("PRICE"));
+			vo.setEa(rs.getString("EA"));
+			vo.setPaymentDate(rs.getString("PAYMENT_DATE"));
+			vo.setExpDate(rs.getString("EXP_DATE"));
+			vo.setLaundryStatus(rs.getString("LAUNDRY_STATUS"));
+			vo.setMemberName(rs.getString("MEMBER_NAME"));
+			vo.setTel(rs.getString("TEL"));
+			vo.setAddress(rs.getString("ADDRESS"));
+			vo.setRequest(rs.getString("REQUEST"));
+		}
 		JDBCTemplate.close(rs);
 		JDBCTemplate.close(pstmt);
 		
-		return orderVoList;
+		return vo;
 	}
 
 }
--- workspace/rushwash/src/main/java/com/rushwash/app/order/service/OrderService.java
+++ workspace/rushwash/src/main/java/com/rushwash/app/order/service/OrderService.java
@@ -1,36 +1,43 @@
 package com.rushwash.app.order.service;
 
 import java.sql.Connection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.rushwash.admin.app.db.util.JDBCTemplate;
 import com.rushwash.app.order.dao.OrderDao;
 import com.rushwash.app.order.vo.OrderVo;
 
-
 public class OrderService {
 
-	public List<OrderVo> getorderList(String memberNo) throws Exception{
-		
+	public List<OrderVo> getorderList(String memberNo) throws Exception {
+
 		Connection conn = JDBCTemplate.getConnection();
-		
+
 		OrderDao dao = new OrderDao();
-		List<OrderVo> orderVoList = dao.getorderList(conn ,memberNo);
-		
+		List<OrderVo> orderVoList = dao.getorderList(conn, memberNo);
+
 		JDBCTemplate.close(conn);
-		
+
 		return orderVoList;
-		
+
 	}
 
-	public OrderVo getorderDetail(String memberNo) throws Exception {
-		
+	public Map<String, Object> getorderDetail(String memberNo) throws Exception {
+
 		Connection conn = JDBCTemplate.getConnection();
-		
+
 		OrderDao dao = new OrderDao();
-	
+		OrderVo vo = dao.getorderDetail(conn, memberNo);
+
 		JDBCTemplate.close(conn);
+		
+		Map<String, Object> map = new HashMap<>();
+		map.put("vo", vo);
+		
+		return map;
+
 	}
-	
 
 }
