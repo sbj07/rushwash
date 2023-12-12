75e92a18ecf7f64141adef471c5413d26cda32a0
zwoo1574 <cherry1574@naver.com>
2023-12-12 12:06:30 +0900

    [게시판]

--- workspace/rushwash/src/main/java/com/rushwash/app/board/faq/dao/FaqDao.java
+++ workspace/rushwash/src/main/java/com/rushwash/app/board/faq/dao/FaqDao.java
@@ -15,7 +15,7 @@ public class FaqDao {
 
 	public List<FaqVo> selectFaqList(Connection conn, PageVo pvo) throws Exception {
 		
-		String sql = "SELECT * FROM( SELECT ROWNUM RNUM, T.* FROM ( SELECT N.NO, N.TITLE , N.CONTENT , M.MANAGER_ID ,TO_CHAR(N.ENROLL_DATE , 'YYYY\"년 \"MM\"월 \"DD\"일\"') AS ENROLL_DATE, N.DEL_YN FROM FAQ N JOIN MANAGER M ON N.MANAGER_NO = M.NO WHERE N.DEL_YN = 'N' ORDER BY NO DESC) T) WHERE RNUM BETWEEN ? AND ?";
+		String sql = "SELECT * FROM( SELECT ROWNUM RNUM, T.* FROM ( SELECT N.NO, N.TITLE , N.CONTENT , M.MANAGER_ID ,TO_CHAR(N.ENROLL_DATE , 'YYYY\"년\"MM\"월\"DD\"일\"') AS ENROLL_DATE, N.DEL_YN FROM FAQ N JOIN MANAGER M ON N.MANAGER_NO = M.NO WHERE N.DEL_YN = 'N' ORDER BY NO ASC) T) WHERE RNUM BETWEEN ? AND ?";
 		PreparedStatement pstmt = conn.prepareStatement(sql);
 		pstmt.setInt(1, pvo.getStartRow());
 		pstmt.setInt(2, pvo.getLastRow());
